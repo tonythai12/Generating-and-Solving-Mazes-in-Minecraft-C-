dd6c509dc7815e9dc385804a7a08bde0dc0e1ece
Ravikaran Singh <s3940606@student.rmit.edu.au>
2023-10-20 13:28:28 +1100

    generating random maze

--- Maze.cpp
+++ Maze.cpp
@@ -6,8 +6,67 @@ Maze::Maze(mcpp::Coordinate basePoint, unsigned int xlen,
                                     unsigned int zlen,
                                     bool mode)
 {
+    this->basePoint = basePoint;
+    this->height = xlen;
+    this->width = zlen;
+    this->mode = mode;
 }
 
 Maze::~Maze()
 {
+
+}
+
+void Maze::generateMazeStructure()
+{
+    // Generate the maze
+    // 1. Initialize the maze
+    for (unsigned int i = 0; i < height; i++)
+    {
+        std::vector<int> temp;
+        for (unsigned int j = 0; j < width; j++)
+        {
+            temp.push_back(0);
+        }
+        maze.push_back(temp);
+    }
+
+    // 2. Generate the maze
+    // 2.1 Generate the random number
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, 1);
+
+    // 2.2 Generate the maze structure
+    for (unsigned int i = 0; i < height; i++)
+    {
+        for (unsigned int j = 0; j < width; j++)
+        {
+            // if the row number is even, then set the wall
+            // if the column number is even, then set the wall
+            if(i % 2 == 0)
+            {
+                maze[i][j] = 1;
+            }
+            else
+            {
+                if(j % 2 == 0)
+                {
+                    maze[i][j] = 1; // 1 means wall
+                }
+            }
+        }
+    }
+
+}
+
+void Maze::generateMaze(){
+    Maze::generateMazeStructure();
+
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++){
+            std::cout << maze[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
--- Maze.h
+++ Maze.h
@@ -13,9 +13,33 @@ public:
                                         bool mode);
     ~Maze();
 
+    // Generate the maze structure
+    void generateMazeStructure();
+
+    // Generate the maze
+    void generateMaze();
+
+    // Print the maze
+    void printMaze();
+
+
 private:
     /* data */
+    // Random length and width of the maze
+    unsigned int height = 7;
+    unsigned int width = 5;
 
+    // The base point of the maze
+    mcpp::Coordinate basePoint;
+
+    // Mode
+    bool mode = false;
+
+    // The maze
+    std::vector<std::vector<int>> maze;
+
+    // BlockType
+    mcpp::BlockType blockType = mcpp::Blocks::BRICKS;
 };
 
 
